97a50987d99411baac11942716566b278249c826
Ryan Davis <ryan@acceleration.net>
2009-11-26 16:36:25 -0500

    Copied in bits of highgui.h and figured out how to pass a CvCapture** after reading up on WTF a foo** means exactly.  Still doesn't work, best guess is a problem with creating the CvNamedWindow or CvShowImage

--- testing.lisp
+++ testing.lisp
@@ -6,11 +6,12 @@
 (in-package :opencv-test)
 
 
- 
+#| 
 (define-foreign-library libcv
   (:unix (:or "libcv.so"))
   (t (error "not implemented")))
 (use-foreign-library libcv)
+|#
 
 (define-foreign-library libhighgui
   (:unix (:or "libhighgui.so"))
@@ -20,63 +21,69 @@
 (defctype cv-capture :pointer)
 (defctype ipl-image :pointer)
 
-(defcfun "cvInitSystem" :int
-  (argv :int)
-  (argc :pointer))
 
+;; start capturing frames from camera: index = camera_index + domain_offset (CV_CAP_*)
+;;CVAPI(CvCapture*) cvCreateCameraCapture( int index );
 (defcfun "cvCreateCameraCapture" cv-capture (index :int))
 
+;; create window
+;;CVAPI(int) cvNamedWindow( const char* name, int flags CV_DEFAULT(CV_WINDOW_AUTOSIZE) );
 (defcfun "cvNamedWindow" :int
   (name :string)
   (flags :int))
 
+;; destroy window and all the trackers associated with it
+;;CVAPI(void) cvDestroyWindow( const char* name );
 (defcfun "cvDestroyWindow" :void
   (name :string))
 
-(defcfun "cvQueryFrame" ipl-image
+;; Just a combination of cvGrabFrame and cvRetrieveFrame
+;;   !!!DO NOT RELEASE or MODIFY the retrieved frame!!!
+;;CVAPI(IplImage*) cvQueryFrame( CvCapture* capture );
+(defcfun "cvQueryFrame" :pointer
   (capture cv-capture))
 
+;; display image within window (highgui windows remember their content)
+;;CVAPI(void) cvShowImage( const char* name, const CvArr* image );
 (defcfun "cvShowImage" :void
   (window :string)
-  (frame ipl-image))
+  (frame :pointer))
 
+;; stop capturing/reading and free resources
+;;CVAPI(void) cvReleaseCapture( CvCapture** capture );
 (defcfun "cvReleaseCapture" :void
   (capture cv-capture))
 
-(defun cam-passthrough (&optional (n 1000))
-  (let ((c (cvcreatecameracapture 0))
-	(name "test"))
-    (if (null-pointer-p c)
-	"no cam"
-	(progn
-	  (cvnamedwindow name 1)
-	(dotimes (i n)
-	  (cvshowimage name
-		       (cvqueryframe c))
-	  )
-	(cvdestroywindow name)
-	(cvreleasecapture (pointer-address c)))))
-  )
+(defun call-with-window (window-name body-fn)
+  (unwind-protect
+       (progn (cvnamedwindow window-name 1)
+	      (funcall body-fn))
+    (cvdestroywindow window-name)))
+
+(defmacro with-window ((window-name) &body body)
+  `(call-with-window ,window-name
+		     #'(lambda () ,@body)))
+
+(defvar *capture*)
+
+(defun cam-passthrough (&optional (n 10))
+  (with-window ("test")
+    (setf *capture* (cvcreatecameracapture 0))
+    (dotimes (i n)
+      (let ((frame (cvqueryframe *capture*)))
+	(break "~a" frame)
+	(cvshowimage "test" frame )))
+    (with-foreign-pointer (cap 1)
+      (setf (mem-ref cap :pointer) *capture*)
+      (cvreleasecapture cap))))
 #|
   CvCapture* capture = cvCaptureFromCAM( CV_CAP_ANY );
-  if( !capture ) {
-    fprintf( stderr, "ERROR: capture is NULL \n" );
-    getchar();
-    return -1;
-  }
-
   // Create a window in which the captured images will be presented
   cvNamedWindow( "mywindow", CV_WINDOW_AUTOSIZE );
-
   // Show the image captured from the camera in the window and repeat
   while( 1 ) {
     // Get one frame
     IplImage* frame = cvQueryFrame( capture );
-    if( !frame ) {
-      fprintf( stderr, "ERROR: frame is null...\n" );
-      getchar();
-      break;
-    }
 
     cvShowImage( "mywindow", frame );
     // Do not release the frame!
