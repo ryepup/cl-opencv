2712cbad4622288d4c11298c19ffc5b45a7632de
Jay Bromley <jbromley@gmail.com>
2011-03-10 02:12:03 -0800

    Added glue functions for cvCopyMakeBorder.
    Added glue function and CV_SCALAR_DECL and CV_SCALAR macros to simplify
    writing code for moving CvScalar structs around.

--- glue/cl-opencv-glue.c
+++ glue/cl-opencv-glue.c
@@ -1,24 +1,26 @@
 /* cl-opencv-glue.c */
 #include "cl-opencv-glue.h"
 
-void cvAbsDiffS_glue(const CvArr *src, CvArr *dest,
-		     double s1, double s2, double s3, double s4)
+void cvAbsDiffS_glue(const CvArr *src, CvArr *dest, CV_SCALAR_DECL(value))
 {
-    cvAbsDiffS(src, dest, cvScalar(s1, s2, s3, s4));
+    cvAbsDiffS(src, dest, CV_SCALAR(value));
 }
 
-void cvSubS_glue(const CvArr *src,
-		 double s1, double s2, double s3, double s4,
-		 CvArr *dest, const CvArr *mask)
+void cvSubS_glue(const CvArr *src, CV_SCALAR_DECL(value), CvArr *dest, 
+		 const CvArr *mask)
 {
-    cvSubS(src, cvScalar(s1, s2, s3, s4), dest, mask);
+    cvSubS(src, CV_SCALAR(value), dest, mask);
 }
 
-void cvSubRS_glue(const CvArr* src, 
-		  double s1, double s2, double s3, double s4, 
-		  CvArr* dest, const CvArr* mask)
+void cvSubRS_glue(const CvArr* src, CV_SCALAR_DECL(value), CvArr* dest, 
+		  const CvArr* mask)
 {
-    cvSubRS(src, cvScalar(s1, s2, s3, s4), dest, mask);
+     cvSubRS(src, CV_SCALAR(value), dest, mask);
 }
 
+void cvCopyMakeBorder_glue(const CvArr* src, CvArr* dst, CvPoint offset, 
+			   int bordertype, CV_SCALAR_DECL(value))
+{
+    cvCopyMakeBorder(src, dst, offset, bordertype, CV_SCALAR(value));
+}
 
--- glue/cl-opencv-glue.h
+++ glue/cl-opencv-glue.h
@@ -4,19 +4,25 @@
  */
 
 #include "cxcore.h"
+#include "cv.h"
+
+#define CV_SCALAR_DECL(x) double x##1, double x##2, double x##3, double x##4
+#define CV_SCALAR(x) cvScalar(x##1, x##2, x##3, x##4)
 
 /* void cvAbsDiffS(const CvArr* src, CvArr* dst, CvScalar value) */
-void cvAbsDiffS_glue(const CvArr *src, CvArr *dest,
-		     double s1, double s2, double s3, double s4);
+void cvAbsDiffS_glue(const CvArr *src, CvArr *dest, CV_SCALAR_DECL(value));
 
 /* void cvSubS(const CvArr* src, CvScalar value, CvArr* dst, 
                const CvArr* mask=NULL) */
-void cvSubS_glue(const CvArr *src,
-		 double s1, double s2, double s3, double s4,
-		 CvArr *dest, const CvArr *mask);
+void cvSubS_glue(const CvArr *src, CV_SCALAR_DECL(value), CvArr *dest,
+		 const CvArr *mask);
 
 /* void cvSubRS(const CvArr* src, CvScalar value, CvArr* dst, 
                 const CvArr* mask=NULL) */
-void cvSubRS_glue(const CvArr* src, 
-		  double s1, double s2, double s3, double s4, 
-		  CvArr* dest, const CvArr* mask);
+void cvSubRS_glue(const CvArr* src, CV_SCALAR_DECL(value), CvArr* dest, 
+		  const CvArr* mask);
+
+/* void cvCopyMakeBorder(const CvArr* src, CvArr* dst, CvPoint offset, 
+                         int bordertype, CvScalar value=cvScalarAll(0)) */
+void cvCopyMakeBorder_glue(const CvArr* src, CvArr* dst, CvPoint offset, 
+			   int bordertype, CV_SCALAR_DECL(value));
